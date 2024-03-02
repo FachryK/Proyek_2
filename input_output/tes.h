c6b6ca0418adb1029023f930f601be81c372574b
Muhammad Fachry Khairiansyah <khairiansyahfachry1@gmail.com>
2024-03-02 17:55:52 +0700

    library data

--- input_output/tes.h
+++ input_output/tes.h
@@ -1,1 +1,19 @@
-#include <iostream>
+#include <string>
+
+// Struktur untuk menyimpan data
+struct Data {
+    std::string NIS;
+    std::string NISN;
+    std::string NAMA;
+    std::string JENIS_KELAMIN;
+    std::string TTL;
+    std::string NIK;
+    std::string NO_KK;
+    std::string AGAMA;
+    std::string ALAMAT_RUMAH;
+    std::string NO_HP;
+    std::string ALAMAT_EMAIL;
+};
+
+// Deklarasi fungsi untuk menyimpan data ke dalam file
+void saveDataToFile(const std::string& filename, const Data& data);
