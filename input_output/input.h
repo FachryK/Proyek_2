39d8a996f4b008c792c0f98002eda59979c43b18
Muhammad Fachry Khairiansyah <khairiansyahfachry1@gmail.com>
2024-03-04 07:51:12 +0700

    perubahan pada input library

--- input_output/input.h
+++ input_output/input.h
@@ -5,7 +5,8 @@
 namespace UserInput {
 
     void inputdata() {
-        std::string NIS, NISN, NAMA, JENIS_KELAMIN, TTL, NIK, NO_KK, AGAMA, ALAMAT_RUMAH, NO_HP, Email;
+        std::string NAMA, JENIS_KELAMIN, TTL, AGAMA, ALAMAT_RUMAH, Email;
+        int NIS, NISN, NIK, No_KK, No_HP;
  
         std::cout << "Masukkan NIS: ";
         std::cin >> NIS;
@@ -22,18 +23,18 @@ namespace UserInput {
         std::cout << "Masukkan NIK: ";
         std::cin >> NIK;
         std::cout << "Masukkan No.KK: ";
-        std::cin >> NO_KK;
+        std::cin >> No_KK;
         std::cout << "Masukkan Agama: ";
         std::cin >> AGAMA;
         std::cout << "Masukkan Alamat Rumah: ";
         std::cin.ignore();
         std::getline(std::cin, ALAMAT_RUMAH);
         std::cout << "Masukkan No.HP: ";
-        std::cin >> NO_HP;
+        std::cin >> No_HP;
         std::cout << "Masukkan Email: ";
         std::cin >> Email;
 
-        std::ofstream outputFile("siswa.txt");
+        std::ofstream outputFile("siswa.txt", std::ios::app);
         if (outputFile.is_open()) {
             outputFile << "NIS: " << NIS << "\n";
             outputFile << "NISN: " << NISN << "\n";
@@ -41,10 +42,10 @@ namespace UserInput {
             outputFile << "Jenis Kelamin: " << JENIS_KELAMIN << "\n";
             outputFile << "TTL: " << TTL << "\n";
             outputFile << "NIK: " << NIK << "\n";
-            outputFile << "No.KK: " << NO_KK << "\n";
+            outputFile << "No.KK: " << No_KK << "\n";
             outputFile << "Agama: " << AGAMA << "\n";
             outputFile << "Alamat Rumah: " << ALAMAT_RUMAH << "\n";
-            outputFile << "No.HP: " << NO_HP << "\n";
+            outputFile << "No.HP: " << No_HP << "\n";
             outputFile << "Email: " << Email << "\n";
             outputFile.close();
             std::cout << "Input data siswa telah disimpan dalam siswa.txt\n";
