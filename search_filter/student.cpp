dc3c1b65a846851ef8edfdbdde2fbf75720596e8
faRkkyyy <fakhrie.rizdan.tif23@polban.ac.id>
2024-03-04 11:09:53 +0700

    membuat pencarian data dan filter

---
+++ search_filter/student.cpp
@@ -0,0 +1,22 @@
+#include "student.h"
+#include "input.h"
+
+// Implementasi kelas Student
+
+// Konstruktor
+Student::Student(const std::string &name, int year)
+    : name(name), year(year)
+{
+}
+
+// Getter untuk nama mahasiswa
+std::string Student::getName() const
+{
+    return name;
+}
+
+// Getter untuk tahun masuk mahasiswa
+int Student::getYear() const
+{
+    return year;
+}
