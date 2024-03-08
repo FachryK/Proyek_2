ccf9e6dcb53729ea3920bdbf54120f85e1b1fb33
Alvito Novandi <alvitonovandi890@gmail.com>
2024-03-08 18:42:31 +0700

    mempebaiki code

--- security/main.c
+++ security/main.c
@@ -1,16 +1,24 @@
 #include "hillcipher_enkripsi.h"
+#include "hillcipher_dekripsi.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int main()
 {
-    int key[2][2] = {{1,2},{3,1}};
+    int key[2][2] = {{3,7},{5,11}};
     char text[10];
 
-    printf("Masukkan Text");
+    printf("Masukkan Text ");
     scanf("%s",text);
 
-    enkripsi_text(text , key);
+    // char *hasil_enkripsi = NULL;
+    enkripsi_text(text/*,hasil_enkripsi*/);
+
+    
+//    free(hasil_enkripsi);
+
+    // printf("dekripsi text");
+    // dekripsi_text(text , key );
 
     return 0;
 }
