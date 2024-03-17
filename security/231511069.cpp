8ab49a63bd51d817b782ad7a430773d25b4a5773
Alvito Novandi <alvitonovandi890@gmail.com>
2024-03-17 22:34:50 +0700

    menghapus enkripsi dan dekripsi bertipe integer dan merapihkan code

--- security/231511069.cpp
+++ security/231511069.cpp
@@ -7,58 +7,21 @@
 #define RANGE_ASCII (ASCII_MAX - ASCII_MIN + 1)
 
 
-
-// Fungsi enkripsi_text dengan overload
-std::string enkripsi_int(int data) {
-    std::ostringstream textStream;
-    textStream << data;
-    std::string text = textStream.str();
-    
-    const unsigned char *input = (const unsigned char *)text.c_str();
-    int panjang_text = text.length();
-    int key[2][2] = {{6,5},{3,4}};
-
-    std::ostringstream hasil_enkripsi;
-
-    // Menambahkan spasi jika panjang_teks bukan kelipatan 2
-    if (panjang_text % 2 != 0) {
-        panjang_text++;
-    }
-
-    for (int i = 0; i < panjang_text; i += 2) {
-        int plain1 = input[i] - ASCII_MIN;
-        int plain2 = input[i + 1] - ASCII_MIN;
-
-        int enkripsi1 = (key[0][0] * plain1 + key[0][1] * plain2) % RANGE_ASCII + ASCII_MIN;
-        int enkripsi2 = (key[1][0] * plain1 + key[1][1] * plain2) % RANGE_ASCII + ASCII_MIN;
-
-        hasil_enkripsi << (char)enkripsi1 << (char)enkripsi2;
-    }
-
-    return hasil_enkripsi.str();
-}
-
-
-std::string enkripsi_text(const std::string &text) {
+std::string enkripsi_text(const std::string &text)
+{
     std::string modifiedText = text;
-    if (text.length() % 2 != 0) {
+    if (text.length() % 2 != 0)
+    {
         modifiedText += '0'; // Menambahkan satu karakter '0' jika panjang teks ganjil
     }
-     const unsigned char *input = (const unsigned char *)modifiedText.c_str();
-     int panjang_text = modifiedText.length();
+    const unsigned char *input = (const unsigned char *)modifiedText.c_str();
+    int panjang_text = modifiedText.length();
 
-//    const unsigned char *input = (const unsigned char *)text.c_str();
-    //int panjang_text = text.length();
-    int key[2][2] = {{6,5},{3,4}};
+    int key[2][2] = {{6,5},{3,4}};//kunci matriks 2x2
 
     std::ostringstream hasil_enkripsi;
 
-    // Menambahkan spasi jika panjang_teks bukan kelipatan 2
-    // if (panjang_text % 2 != 0)
-    // {
-    //     panjang_text++;
-    // }
-
+    //proses enkripsi
     for (int i = 0; i < panjang_text; i += 2)
     {
         int plain1 = input[i] - ASCII_MIN;
@@ -67,19 +30,22 @@ std::string enkripsi_text(const std::string &text) {
         int enkripsi1 = (key[0][0] * plain1 + key[0][1] * plain2) % RANGE_ASCII + ASCII_MIN;
         int enkripsi2 = (key[1][0] * plain1 + key[1][1] * plain2) % RANGE_ASCII + ASCII_MIN;
 
-        hasil_enkripsi << (char)enkripsi1 << (char)enkripsi2;
+        hasil_enkripsi << (char)enkripsi1 << (char)enkripsi2; //menampung hasil enkripsi
     }
 
-    return hasil_enkripsi.str();
+    return hasil_enkripsi.str(); //hasil enkripsi dikembalikan sebagai string
 }
 
-std::string dekripsi_text(const std::string &text) {
+std::string dekripsi_text(const std::string &text)
+{
     const unsigned char *input = (const unsigned char *)text.c_str();
     int panjang_text = text.length();
-    int invkey[2][2] = {{11, 10}, {63, 64}};
+    int invkey[2][2] = {{11, 10}, {63, 64}}; //invers kunci 2x2
     std::ostringstream hasil_dekripsi;
 
-    for (int i = 0; i < panjang_text; i += 2) {
+    //proses dekripsi
+    for (int i = 0; i < panjang_text; i += 2) 
+    {
         int ciper1 = input[i] - ASCII_MIN;
         int ciper2 = input[i + 1] - ASCII_MIN;
 
@@ -88,40 +54,12 @@ std::string dekripsi_text(const std::string &text) {
 
         hasil_dekripsi << (char)dekripsi1 << (char)dekripsi2;
     }
-       std::string hasil = hasil_dekripsi.str();
+    std::string hasil = hasil_dekripsi.str();
+
     // Hapus karakter '0' terakhir jika ada
-    if (hasil.length() > 0 && hasil[hasil.length() - 1] == '0') {
+    if (hasil.length() > 0 && hasil[hasil.length() - 1] == '0') 
+    {
         hasil.erase(hasil.length() - 1);
     }
     return hasil;
-}
-
-std::string dekripsi_int(int data) {
-    std::ostringstream textStream;
-    textStream << data;
-    std::string text = textStream.str();
-    
-    const unsigned char *input = (const unsigned char *)text.c_str();
-    int panjang_text = text.length();
-    int invkey[2][2] = {{11, 10}, {63, 64}};
-    std::ostringstream hasil_dekripsi;
-
-    for (int i = 0; i < panjang_text; i += 2) {
-        int ciper1 = input[i] - ASCII_MIN;
-        int ciper2 = input[i + 1] - ASCII_MIN;
-
-        int dekripsi1 = (invkey[0][0] * ciper1 + invkey[0][1] * ciper2) % RANGE_ASCII + ASCII_MIN;
-        int dekripsi2 = (invkey[1][0] * ciper1 + invkey[1][1] * ciper2) % RANGE_ASCII + ASCII_MIN;
-
-        hasil_dekripsi << (char)dekripsi1 << (char)dekripsi2;
-    }
-     std::string hasil = hasil_dekripsi.str();
-    // Hapus karakter '0' terakhir jika ada
-    if (hasil.length() > 0 && hasil[hasil.length() - 1] == '0') {
-        hasil.erase(hasil.length() - 1);
-    }
-
-    return hasil;
-
-    //return hasil_dekripsi.str();
 }
