de8fd79538518935e5158f4e394baada451fc6dc
Alvito Novandi <alvitonovandi890@gmail.com>
2024-03-17 22:42:51 +0700

    menghapus fungsi enkripsi dan dekripsi

--- security/231511069.h
+++ security/231511069.h
@@ -1,16 +1,10 @@
 #ifndef HILLCIPHER_ENKRIPSI_H
 #define HILLCIPHER_ENKRIPSI_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 
-std::string enkripsi_int(int data);
 std::string enkripsi_text(const std::string &text);
-
 std::string dekripsi_text(const std::string &text);
-std::string dekripsi_int(int data);
 
 
 #endif /* HILLCIPHER_ENKRIPSI_H */
